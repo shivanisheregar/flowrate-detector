53342b4bdd9ed0a46e7530ce0679383bb7e4a5cd
shivani sheregar <shivanisheregar02@gmail.com>
2026-06-14 16:02:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 
-print("Bubble Flow Meter Analyzer Started")
+from src.flow_calculator import calculate_flow_rate
+
+volume = 1000
+time_taken = 5
+
+flow_rate = calculate_flow_rate(volume, time_taken)
+
+print("Volume:", volume, "mL")
+print("Time:", time_taken, "s")
+print("Flow Rate:", flow_rate, "mL/s")
